93dc9b3adfaf485bd450d3a0a11463811a577a80
Diljot Garcha <diljotsg@gmail.com>
2020-04-27 13:37:23 -0500

    Bug fixes

--- src/services/oracle.py
+++ src/services/oracle.py
@@ -1,4 +1,5 @@
 from datetime import date
+from datetime import MAXYEAR
 from datetime import timedelta
 from src.services.tmdb import TMDBService
 from src.constants.values import TMDB_MCU_LIST
@@ -9,6 +10,7 @@ from typing import Optional
 class Oracle:
     def __init__(self):
         self.tmdb: TMDBService = TMDBService()
+        self.max_date = "{}-01-01".format(MAXYEAR)
 
     def get_next_movie(
         self,
@@ -25,10 +27,13 @@ class Oracle:
                 # Get the current date in ISO format
                 desired_date = date.today().isoformat()
 
-            # Find the first film with a release date larger than the current date
-            index: int = next(i for i, v in enumerate(movie_list) if v["release_date"] > desired_date)
-            if index:
-                return movie_list[index]
+            try:
+                # Find the first film with a release date larger than the current date
+                index: int = next(i for i, v in enumerate(movie_list) if v.get("release_date", self.max_date) > desired_date)
+                if index:
+                    return movie_list[index]
+            except StopIteration:
+                pass
 
         return None
 
@@ -47,11 +52,11 @@ class Oracle:
 
         if next_movie:
             # Days until the movies' release
-            days_until: timedelta = date.fromisoformat(next_movie["release_date"]) - date.today()
+            days_until: timedelta = date.fromisoformat(next_movie.get("release_date", self.max_date)) - date.today()
 
             # Format the result dictionary
             result["title"] = next_movie["original_title"]
-            result["release_date"] = next_movie["release_date"]
+            result["release_date"] = next_movie.get("release_date", self.max_date)
             result["poster_path"] = self.tmdb.give_poster_url(next_movie["poster_path"])
             result["overview"] = next_movie["overview"]
             result["days_until"] = int(days_until.days)
